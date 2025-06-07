5912913b3dfd58b506b79444a4cb9adfb257a799
Lutz Christoph <Christoph.Lutz@swisscom.com>
2025-06-07 12:29:37 +0200

    Use vars.sql to make connection

--- 19.23/sql/connect-sdu.sql
+++ 19.23/sql/connect-sdu.sql
@@ -1,3 +1,7 @@
+-- Connect to a service using a sdu size of 1234
+-- Note: requires a vars.sql file with the appropriate
+-- variables defined.
+@vars.sql
 
-prompt connect test/******@...?SDU=1234
-connect test/TesT_012345$67890@stdm02db01-v.corproot.net:1521/TPDB002_RW.test.dbs?SDU=1234
+prompt connect &&test_user/********@...:1521/&&pdb..&&test_domain
+connect &&test_user/&&test_user_pwd@&&test_vip:1521/&&pdb..&&test_domain
